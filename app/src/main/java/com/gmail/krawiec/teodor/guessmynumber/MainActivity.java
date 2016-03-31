a2ef52fd7d5fa5e99b16857511f91f7bdba57f13
Theo85 <teodor.krawiec@gmail.com>
2016-03-31 17:02:25 +0200

    Bug fix

--- app/src/main/java/com/gmail/krawiec/teodor/guessmynumber/MainActivity.java
+++ app/src/main/java/com/gmail/krawiec/teodor/guessmynumber/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,8 +13,9 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int myNumber = 50, userNumber, guessCount = 0, numberRange = 100;
-    String listOfGuesses = "", massage;
+    int myNumber, userNumber, guessCount = 0, numberRange = 100;
+    String listOfGuesses = "";
+    boolean gameWon = false;
     TextView textView;
     TextView textView2;
     TextView textView3;
@@ -40,53 +40,53 @@ public class MainActivity extends AppCompatActivity {
         textView3.setText("Second best score: " + sharedPreferences.getInt("SecondBestScore", 100));
         numberRange = sharedPreferences.getInt("NumberRange", 100);
         myNumber = rand.nextInt(numberRange) + 1;
-        EditText editText2 = (EditText)findViewById(R.id.editText2);
-        editText2.setText(numberRange+"");
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        editText2.setText(numberRange + "");
     }
 
     public void takeTheGuess(View view) {
         EditText editText = (EditText) findViewById(R.id.editText);
-
-        if (editText.getText().toString().equals("")) {
-
-        } else {
-            guessCount++;
-            userNumber = Integer.parseInt(editText.getText().toString());
-            String massage;
-
-            if (userNumber > myNumber) {
-                massage = "My number is lesser than yours.";
-                textView5.setText("Last guess was: too big");
-            } else if (userNumber < myNumber) {
-                massage = "My number is bigger than yours.";
-                textView5.setText("Last guess was: too small");
-            } else {
-                massage = "Congrats! You guessed my number";
-                textView5.setText("You nailed it! My number is " + myNumber);
-                if (guessCount < sharedPreferences.getInt("BestScore", 100)) {
-                    editor.putInt("SecondBestScore", sharedPreferences.getInt("BestScore", 100));
-                    editor.putInt("BestScore", guessCount);
-                    editor.commit();
-                    textView2.setText("Best score: " + sharedPreferences.getInt("BestScore", 100));
-                    textView3.setText("Second best score: " + sharedPreferences.getInt("SecondBestScore", 100));
-                } else if (guessCount < sharedPreferences.getInt("SecondBestScore", 100)) {
-                    editor.putInt("SecondBestScore", guessCount);
-                    editor.commit();
-                    textView3.setText("Second best score: " + sharedPreferences.getInt("SecondBestScore", 100));
-                }
-            }
-
-            Context context = getApplicationContext();
-            int duration = Toast.LENGTH_SHORT;
-            Toast toast = Toast.makeText(context, massage, duration);
-            toast.show();
-
-            textView.setText("Times guessed: " + guessCount);
-            listOfGuesses += userNumber + ", ";
-            editor.putString("GuessList", "Your guesses: " + listOfGuesses);
-            editor.commit();
-            textView4.setText(sharedPreferences.getString("GuessList", "error"));
+        if (gameWon){
+            displayMassage("It is time to start new game.");
             editText.setText(null);
+        } else {
+            if (editText.getText().toString().equals("")) {
+                displayMassage("No number provided.");
+            } else if (Integer.parseInt(editText.getText().toString()) == 0 || Integer.parseInt(editText.getText().toString()) > sharedPreferences.getInt("NumberRange",100)){
+                displayMassage("Number out of range.");
+                editText.setText(null);
+            } else {
+                guessCount++;
+                userNumber = Integer.parseInt(editText.getText().toString());
+                if (userNumber > myNumber) {
+                    displayMassage("Your number is too big.");
+                    textView5.setText("Last guess was: too big.");
+                } else if (userNumber < myNumber) {
+                    displayMassage("Your number is too small.");
+                    textView5.setText("Last guess was: too small.");
+                } else {
+                    displayMassage("Congrats! You guessed my number!");
+                    gameWon = true;
+                    textView5.setText("You nailed it! My number is " + myNumber);
+                    if (guessCount < sharedPreferences.getInt("BestScore", 100)) {
+                        editor.putInt("SecondBestScore", sharedPreferences.getInt("BestScore", 100));
+                        editor.putInt("BestScore", guessCount);
+                        editor.commit();
+                        textView2.setText("Best score: " + sharedPreferences.getInt("BestScore", 100));
+                        textView3.setText("Second best score: " + sharedPreferences.getInt("SecondBestScore", 100));
+                    } else if (guessCount < sharedPreferences.getInt("SecondBestScore", 100)) {
+                        editor.putInt("SecondBestScore", guessCount);
+                        editor.commit();
+                        textView3.setText("Second best score: " + sharedPreferences.getInt("SecondBestScore", 100));
+                    }
+                }
+                textView.setText("Times guessed: " + guessCount);
+                listOfGuesses += userNumber + ", ";
+                editor.putString("GuessList", "Your guesses: " + listOfGuesses);
+                editor.commit();
+                textView4.setText(sharedPreferences.getString("GuessList", "error"));
+                editText.setText(null);
+            }
         }
     }
 
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         resetBestScoreCode();
     }
 
-    public void resetBestScoreCode(){
+    public void resetBestScoreCode() {
         editor.putInt("BestScore", 100);
         editor.putInt("SecondBestScore", 100);
         editor.commit();
@@ -104,13 +104,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void setCustomRange(View view) {
         EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!editText2.getText().toString().equals("") && !editText2.getText().toString().equals("0")) {
-            numberRange = Integer.parseInt(editText2.getText().toString());
-            editor.putInt("NumberRange", numberRange);
-            newGameCode();
-            resetBestScoreCode();
+        if (Integer.parseInt(editText2.getText().toString()) == sharedPreferences.getInt("NumberRange", 100)){
+            displayMassage("The range remains not changed.");
         } else {
-            //   Toast toast = new Toast.makeText();
+            if (!editText2.getText().toString().equals("") && !editText2.getText().toString().equals("0")) {
+                numberRange = Integer.parseInt(editText2.getText().toString());
+                editor.putInt("NumberRange", numberRange);
+                newGameCode();
+                resetBestScoreCode();
+            } else {
+                editText2.setText(sharedPreferences.getInt("NumberRange", 100) + "");
+                displayMassage("Enter number higher than 0.");
+            }
         }
     }
 
@@ -121,12 +126,20 @@ public class MainActivity extends AppCompatActivity {
     public void newGameCode() {
         myNumber = rand.nextInt(numberRange) + 1;
         guessCount = 0;
+        gameWon = false;
         listOfGuesses = "";
+        EditText editText = (EditText) findViewById(R.id.editText);
+        editText.setText(null);
         textView.setText("Times guessed: " + guessCount);
         editor.putString("GuessList", "Your guesses: ");
         editor.commit();
         textView4.setText(sharedPreferences.getString("GuessList", "error"));
         textView5.setText("Last guess was: ");
-    //save me
+    }
+    public void displayMassage(String massage){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, massage, duration);
+        toast.show();
     }
 }
